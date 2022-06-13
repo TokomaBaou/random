99a719792d9c0d3dc19f84f26114592cd1e208d9
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-06-14 00:33:03 +0900

    switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import "./styles.css";
 
-export default function App() {
-  let com = Math.floor(Math.random() * 3) + 1;
-  console.log(com);
+export const App = () => {
+  let com_jan = Math.floor(Math.random() * 3) + 1;
+  console.log(com_jan);
+
+  let user_jan = "";
+
+  switch (user_jan) {
+    case 1:
+      user_jan = "ぐー";
+      break;
+    case 2:
+      user_jan = "ちょき";
+      break;
+    case 3:
+      user_jan = "ぱー";
+      break;
+    // default:
+    //   console.log("１から3の値を入力してください");
+  }
 
   return (
     <div className="App">
@@ -10,4 +26,4 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
+};
